ff08fcee2a44684f17e612c2d7f452b473e33582
RahidBot <139769340+RahidBot@users.noreply.github.com>
2024-02-04 16:58:02 +0400

    update thumbnails.py

--- VenomX/utils/thumbnails.py
+++ VenomX/utils/thumbnails.py
@@ -123,7 +123,7 @@ async def get_thumb(videoid):
 
             image4 = ImageDraw.Draw(image2)
             image4.text((10, 10), "Rahid", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+            image4.text((670, 150), "Oxunur...", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
             title1 = truncate(title)
@@ -131,9 +131,9 @@ async def get_thumb(videoid):
             image4.text((670, 350), text=title1[1], fill="white", stroke_width=1, stroke_fill="white", font = font3, align ="left") 
 
             # description
-            views = f"Views : {views}"
-            duration = f"Duration : {duration} Mins"
-            channel = f"Channel : {channel}"
+            views = f"Baxış sayı: {views}"
+            duration = f"Müddət: {duration}"
+            channel = f"Kanal: {channel}"
 
             image4.text((670, 450), text=views, fill="white", font = font4, align ="left") 
             image4.text((670, 500), text=duration, fill="white", font = font4, align ="left") 
